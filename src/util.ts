816e5e52fff6784df908f77a7f32255f59b8f372
Burner <burner@burner.gay>
2022-07-23 13:41:23 -0500

    Removed dead code

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,5 @@
-import { Bindings } from './types.d'
 import Base64url from 'crypto-js/enc-base64url'
 import Utf8 from 'crypto-js/enc-utf8'
-import { Context } from 'hono'
 
 export const Base64URL = {
   encode: (str: string) => Base64url.stringify(Utf8.parse(str)),
@@ -11,13 +9,4 @@ export const Base64URL = {
 export function randNum(length: number) {
   const num = Math.floor(Math.random() * Math.pow(10, length))
   return num.toString().padStart(length, '0')
-}
-
-export async function hasLink<T extends string>(
-  ctx: Context<T, Bindings>,
-  id: string,
-) {
-  const { LINKS } = ctx.env
-  const result = await LINKS.get(id)
-  return result ? true : false
 }
