c76db7ad5904b6cb8dd2c8bb31e4b89ba20f7aa6
Burner <burner@burner.gay>
2022-07-25 16:23:09 -0500

    Changes to robots.txt

--- src/strings.ts
+++ src/strings.ts
@@ -1,5 +1,12 @@
 export const ROBOTS_TXT = `
 # This is a link shortener go away
-User-agent: *
+User-agent: Googlebot
+User-agent: Googlebot-Image
+User-agent: Googlebot-News
+User-agent: Googlebot-Video
+User-agent: AdsBot-Google
+User-agent: Bingbot
+User-agent: MSNBot-Media
+User-agent: Baiduspider
 Disallow: /
 `
