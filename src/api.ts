0673ffc5e3cfa3381dbf5aa29317d20d0d23f73a
Burner <burner@burner.gay>
2022-08-11 19:48:45 -0500

    Moved API into a separate router

---
+++ src/api.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono'
+import { Base64URL, randNum } from './util'
+
+// The internal API is expected to be protected by Cloudflare Access, so
+// no authentication is implemented.
+const api = new Hono<Bindings>()
+
+api.post('/add', async (ctx) => {
+  const link = await ctx.req.json<Link>()
+  if (link.name) {
+    ctx.env.LINKS.put(link.name, link.url)
+    return ctx.json({
+      status: 'ok',
+      link: `/${link.name}`,
+      redirect: link.url,
+    })
+  } else {
+    let id = Base64URL.encode(randNum(7))
+    // Ensure that the id is unique
+    while (await ctx.env.LINKS.get(id)) {
+      id = Base64URL.encode(randNum(7))
+    }
+    ctx.env.LINKS.put(id, link.url)
+    return ctx.json({
+      status: 'ok',
+      link: `/${id}`,
+      redirect: link.url,
+    })
+  }
+})
+
+export default api
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Context, Hono } from 'hono'
-// import { basicAuth } from 'hono/basic-auth'
 import { logger } from 'hono/logger'
 import { prettyJSON } from 'hono/pretty-json'
+import api from './api'
 import { ROBOTS_TXT } from './strings'
-import { Base64URL, randNum } from './util'
 
 const app = new Hono<Bindings>()
 
@@ -29,38 +28,6 @@ function redirectGen<T extends string>(id_name: T) {
 app.get('/:id', redirectGen('id'))
 app.get('/go/:id2', redirectGen('id2'))
 
-app.post(
-  '/api/add',
-  // async (ctx, next) => {
-  //   const auth = basicAuth({
-  //     username: ctx.env.USERNAME,
-  //     password: ctx.env.PASSWORD,
-  //   })
-  //   await auth(ctx, next)
-  // },
-  async (ctx) => {
-    const link = await ctx.req.json<Link>()
-    if (link.name) {
-      ctx.env.LINKS.put(link.name, link.url)
-      return ctx.json({
-        status: 'ok',
-        link: `/${link.name}`,
-        redirect: link.url,
-      })
-    } else {
-      let id = Base64URL.encode(randNum(7))
-      // Ensure that the id is unique
-      while (await ctx.env.LINKS.get(id)) {
-        id = Base64URL.encode(randNum(7))
-      }
-      ctx.env.LINKS.put(id, link.url)
-      return ctx.json({
-        status: 'ok',
-        link: `/${id}`,
-        redirect: link.url,
-      })
-    }
-  },
-)
+app.route('/api', api)
 
 export default app
